15b16dcb373c995e2d8499905400593cb6b015a6
YatiHikk <66408441+YatiHikk@users.noreply.github.com>
2022-09-29 21:57:02 +0300

    New Commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded',() =>{
+  console.log(1)
+  window.addEventListener('scroll', function (){
+    if(pageYOffset != 0) {
+      document.querySelector('header').classList.add('__closed')
+    }else {
+      document.querySelector('header').classList.remove('__closed')
+    }
+  })
+})
+
