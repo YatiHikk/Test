91082b4ea30276279dad1545c34ee550fe4f7730
YatiHikk <66408441+YatiHikk@users.noreply.github.com>
2022-09-30 07:12:28 +0300

    New Commit
    Format

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded',() =>{
+document.addEventListener('DOMContentLoaded', () => {
   console.log(1)
-  window.addEventListener('scroll', function (){
-    if(pageYOffset != 0) {
+  window.addEventListener('scroll', function () {
+    if (pageYOffset != 0) {
       document.querySelector('header').classList.add('__closed')
-    }else {
+    } else {
       document.querySelector('header').classList.remove('__closed')
     }
   })
